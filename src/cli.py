7f504b57cdf198b7c08bb1717a72fe7baa1bf167
kweezl <20753596+kweezl@users.noreply.github.com>
2026-06-28 22:07:31 +0400

    feat: add parse-factions parser and command

--- src/cli.py
+++ src/cli.py
@@ -8,6 +8,7 @@ from src.commands import (
     generate_icons,
     parse_contracts,
     parse_craft,
+    parse_factions,
     parse_item_categories,
     parse_items,
     parse_space_objects,
@@ -40,6 +41,7 @@ app.command("parse-workshops")(parse_workshops.command)
 app.command("parse-contracts")(parse_contracts.command)
 app.command("parse-item-categories")(parse_item_categories.command)
 app.command("parse-space-objects")(parse_space_objects.command)
+app.command("parse-factions")(parse_factions.command)
 app.command("parse-translations")(parse_translations.command)
 app.command("generate-icons")(generate_icons.command)
 app.command("deduplicate-icons")(deduplicate_icons.command)
